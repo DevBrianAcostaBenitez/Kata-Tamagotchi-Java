29340bc10e9f07264b3a42eaa2ce081840130e45
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-07-15 15:24:43 +0100

    functionality to play,sleep, eat, poop and passage of time and tests done

--- src/main/java/com/kata/Tamagotchi.java
+++ src/main/java/com/kata/Tamagotchi.java
@@ -1,9 +1,94 @@
 package com.kata;
-
 public class Tamagotchi {
+    private int mood = 4;
+    private int hunger = 4;
+    private int energy = 4;
+    private int fullness = 4;
+
+    public void play() {
+        if (energy !=0){
+            energy -= 1;
+            hunger += 1;
+            mood += 1;
+        }else{
+           System.out.println("your tamagotchi is too sleepy, let your tamagotchi sleep"); 
+        }
+    }
     
-    int mood = 4;
-    int hunger = 4;
-    int energy = 4;
+    public void feed() {
+        if (hunger>1){
+            hunger -= 2;
+            energy += 1;
+            fullness +=1;
+        }else{
+            System.out.println("your tamagotchi is not hungry"); 
+        }
+    }
+    
+    public void sleep() {
+        if (hunger!=0){
+            energy += 2;
+            hunger += 1;
+        }else{
+            System.out.println("your tamagotchi is too hungry to sleep"); 
+        }
+    }
+    public void poop() {
+        fullness -=3;
+        if (fullness <0){
+            fullness =0;
+        }
+    }
+    public void timePasses() {
+        if (energy>0){
+            energy -= 1;
+        }
+        if (mood>0){
+            mood -= 1;
+        }
+        if (hunger>0){  
+            hunger += 1;
+        }
+    }
+    public void checkStats() {
+        System.out.println("Current stats:");
+        System.out.println("Mood: " + getMoodStat());
+        System.out.println("Hunger: " + hunger);
+        System.out.println("Energy: " + getEnergyStat());
+        System.out.println("Fullness: " + fullness);
+    }
+    
+    public String getMoodStat() {
+        if (mood > 8) {
+            return ":-)";
+        } else if (mood < 2) {
+            return "ఠ_ఠ";
+        } else {
+            return ":-|";
+        }
+    }
+     public String getEnergyStat() {
+        if (energy < 3) {
+            return "(-_-)";
+        } else if (energy == 0) {
+            return "(-_-) zZZ";
+        } else {
+            return ":-|";
+        }
+    }
+    public int getHungerStat() {
+        return hunger;
+    }
+    public int getFullnessStat() {
+        return fullness;
+    }
+    public void setStats(int newMood, int newHunger, int newEnergy, int newFullness){
+        mood=newMood;
+        hunger= newHunger;
+        energy=newEnergy;
+        fullness=newFullness;
+    }
+    
+
 
 }
--- src/test/java/com/kata/TamagotchiTest.java
+++ src/test/java/com/kata/TamagotchiTest.java
@@ -1,15 +1,63 @@
 package com.kata;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TamagotchiTest {
+    private Tamagotchi tamagotchi;
+    
+    @Before
+    public void setUp() {
+        tamagotchi = new Tamagotchi();
+        
+        tamagotchi.setStats(8, 5, 3, 5);
+    }
     
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+    public void testPlay() {
+        tamagotchi.play();
 
+        assertEquals("Mood should increase by 1 after playing", ":-)", tamagotchi.getMoodStat());
+        assertEquals("Hunger should increase by 1 after playing", 6, tamagotchi.getHungerStat());
+        assertEquals("Energy should decrease by 1 after playing", "(-_-)", tamagotchi.getEnergyStat());
+    }
+    
+    @Test
+    public void testFeed() {
+        tamagotchi.feed();
+
+        assertEquals("Hunger should decrease by 2 after feeding", 3, tamagotchi.getHungerStat());
+        assertEquals("Energy should increase by 1 after feeding", ":-|", tamagotchi.getEnergyStat());
+        assertEquals("Fullness should increase by 1 after feeding", 6, tamagotchi.getFullnessStat());
+    }
+    
+    @Test
+    public void testSleep() {
+        tamagotchi.sleep();
+
+        assertEquals("Energy should increase by 2 after sleeping", ":-|", tamagotchi.getEnergyStat());
+        assertEquals("Hunger should increase by 1 after sleeping", 6, tamagotchi.getHungerStat());
+    }
+    
+    @Test
+    public void testPoop() {
+        tamagotchi.poop();
+        
+        assertEquals("Fullness should decrease by 3 after pooping", 2, tamagotchi.getFullnessStat());
+
+        tamagotchi.poop();
+        
+        assertEquals("Fullness should be 0 if already less than 3", 0, tamagotchi.getFullnessStat());
+    }
+    
+    @Test
+    public void testTimePasses() {
+        tamagotchi.timePasses();
+        
+        assertEquals("Energy should decrease by 1 after time passes", "(-_-)", tamagotchi.getEnergyStat());
+        assertEquals("Mood should decrease by 1 after time passes", ":-|", tamagotchi.getMoodStat());
+        assertEquals("Hunger should increase by 1 after time passes", 6, tamagotchi.getHungerStat());
+    }
 }
